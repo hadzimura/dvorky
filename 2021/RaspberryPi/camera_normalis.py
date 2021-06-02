d5c1492bb8c6716b07501d7e0e3e7834c7c04479
Gitlab-Runner iMac.local <su_runner@iMac.local>
2021-06-02 12:02:53 +0200

    Relay class crated

--- 2021/RaspberryPi/camera_normalis.py
+++ 2021/RaspberryPi/camera_normalis.py
@@ -4,7 +4,54 @@
 from Display import MiniDisplay
 from Controller import AKAI_LPD8_MIDI
 import mido
+import RPi.GPIO as GPIO
 import time
+
+
+class Relay(object):
+	
+	def __init__(self, pinout):
+		
+		# Setting a current GPIO mode
+		GPIO.setmode(GPIO.BCM)
+		
+		# Removing the warnings
+		GPIO.setwarnings(False)
+		
+		# creating a list (array) with the number of GPIO's that we use
+		self.pins = pinout
+		
+		# setting the mode for all pins so all will be switched on
+		GPIO.setup(self.pins, GPIO.OUT)
+		
+		# for loop where pin = 18 next 17 ,15, 14
+		for pin in self.pins:
+			# setting the GPIO to HIGH or 1 or true
+			GPIO.output(pin, GPIO.HIGH)
+			# wait 0,5 second
+			time.sleep(0.5)
+			# setting the GPIO to LOW or 0 or false
+			GPIO.output(pin, GPIO.LOW)
+			# wait 0,5 second
+			time.sleep(0.5)
+			
+			# Checking if the current relay is running and printing it
+			if not GPIO.input(pin):
+				print("Pin " + str(pin) + " is working")
+		
+		# same but the difference is that  we have
+		# for loop where pin = 14 next 15,17,18
+		# backwards
+		for pin in reversed(self.pins):
+			GPIO.output(pin, GPIO.HIGH)
+			time.sleep(0.5)
+			
+			GPIO.output(pin, GPIO.LOW)
+			time.sleep(0.5)
+		
+		# cleaning all GPIO's
+		GPIO.cleanup()
+		print("Shutdown All relays")
 
 
 class CameraNormalis(object):
@@ -51,6 +98,9 @@ class CameraNormalis(object):
 
 if __name__ == '__main__':
 
+	r = Relay([18, 17, 15, 14])
+	exit()
+	
 	app = CameraNormalis(midi_name='LPD8')
 	app.test_midi()
 
